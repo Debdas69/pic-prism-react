b3a8cf4965373d13ad9fa1ebd92891b38cfd6c96
Debdas69 <deb05838@gmail.com>
2024-11-09 00:06:13 +0530

    day 1 completed

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import { IoIosSearch } from "react-icons/io";
+
+const HeroSection = () => {
+  return (
+    <div className="sm:w-[60vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center mt-20">
+
+      <form className="absolute flex justify-center items-center">
+        <input type="text"
+        id="search"
+        name="search"
+        className="py-4 px-3 w-[80vw] text-xl sm:text-3xl mx-auto outline-none border-b-2 "
+        placeholder="Search your asset..."
+        />
+        <IoIosSearch className="text-3xl sm:text-5xl text-gray-400 -ml-12"/>
+      </form>
+    </div>
+  )
+}
+
+export default HeroSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import HeroSection from "../components/HeroSection"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+
+      <HeroSection/>
+    </>
   )
 }
 
